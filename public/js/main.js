8e77e86176723961896ca9730d8672aa491cee7b
rmfnxm23 <shpark5733@gmail.com>
2025-03-11 22:50:17 +0900

    Feat: #24 commit (내용사라짐)

--- public/js/main.js
+++ public/js/main.js
@@ -86,6 +86,32 @@ showText = (detail, maxLength) => {
   }
 };
 
+// 수정한 날짜를 나타내는 형식
+function timeForToday(value) {
+  const today = new Date();
+  const timeValue = new Date(value);
+
+  const betweenTime = Math.floor(
+    (today.getTime() - timeValue.getTime()) / 1000 / 60
+  );
+  if (betweenTime < 1) return "방금전";
+  if (betweenTime < 60) {
+    return `${betweenTime}분전`;
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60);
+  if (betweenTimeHour < 24) {
+    return `${betweenTimeHour}시간전`;
+  }
+
+  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+  if (betweenTimeDay < 1) {
+    return `${betweenTimeDay}일전`;
+  }
+
+  return timeValue.toLocaleDateString(); // new Date 형식으로 날짜 표시
+}
+
 // 카테고리 별 게시글 보기
 const categoryType = (categoryId) => {
   const postWrap = document.querySelector(".postWrap");
